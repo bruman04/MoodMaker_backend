505063c3df6682ffcd3b4fe5e24f4665fda92347
clara.aw.2021 <clara.aw.2021@scis.smu.edu.sg>
2024-07-08 08:54:29 +0800

    config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-DB_USERNAME = os.getenv('DB_USERNAME', '')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_USERNAME = os.getenv('DB_USERNAME', 'roy')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'M00dmaker$2k24')
+
 DB_HOST = os.getenv('DB_HOST', 'moodmaker-video-metadata-1.cviss0oyav5u.ap-southeast-2.rds.amazonaws.com')
 DB_PORT = os.getenv('DB_PORT', '5432')
 DB_NAME = os.getenv('DB_NAME', 'moodmaker-video-metadata-1')
